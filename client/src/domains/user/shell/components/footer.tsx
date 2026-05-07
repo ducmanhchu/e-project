a46b933c56bb29b16114feb84937b2d97c6e2144
Manh Chu <ducmanhchu0704@gmail.com>
2026-05-08 04:06:42 +0700

    [FE] refactor: change style for the landing page and footer.

--- client/src/domains/user/shell/components/footer.tsx
+++ client/src/domains/user/shell/components/footer.tsx
@@ -1,34 +1,29 @@
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Moon02Icon, Sun01Icon } from "@hugeicons/core-free-icons";
+import { ArrowUp02Icon } from "@hugeicons/core-free-icons";
+
 import { Logo } from "@shared/components/logo";
-import { useTheme } from "@shared/hooks/use-theme";
 import { Button } from "@shared/components/ui/button";
 
 export function Footer() {
-	const { theme, toggleTheme } = useTheme();
-
 	return (
-		<footer className="flex justify-between items-center rounded-t-4xl w-full bg-primary text-primary-foreground p-4 md:px-10 lg:px-20">
+		<footer className="flex justify-between items-center rounded-t-4xl w-full bg-secondary-black text-secondary-white p-4 md:px-10 lg:px-20">
 			<div className="flex flex-col justify-start">
 				<Logo className="h-14 w-32" />
-				<p className="text-sm text-muted-foreground">
+				<p className="text-sm font-light text-secondary-white">
 					Copyright © 2026. All rights reserved.
 				</p>
 			</div>
-			<div className="flex items-center gap-2">
-				<Button
-					variant="ghost"
-					size="icon"
-					aria-label="Chuyển đổi chủ đề sáng/tối"
-					onClick={toggleTheme}
-				>
-					{theme === "light" ? (
-						<HugeiconsIcon icon={Moon02Icon} />
-					) : (
-						<HugeiconsIcon icon={Sun01Icon} />
-					)}
-				</Button>
-			</div>
+			<Button
+				className="group bg-secondary-white hover:bg-secondary-black border border-transparent hover:border-secondary-white transition-all duration-200"
+				size="icon"
+				onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+				aria-label="Scroll to top"
+			>
+				<HugeiconsIcon
+					icon={ArrowUp02Icon}
+					className="text-secondary-black transition-colors duration-200 group-hover:text-secondary-white"
+				/>
+			</Button>
 		</footer>
 	);
 }
